6e47d85228bf04d91af722b2c69429accc20074d
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-26 08:27:18 -0800

    Current user and log out (#3)
    * Create route for current_user
    
    * Code cleanup
    
    * Add user_id to serializer
    
    * Code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :current_user, only: :show, controller: :current_user
+
   resources :snacks, only: [:index]
 end
