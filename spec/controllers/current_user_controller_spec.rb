57eeb4a7fb05eabae5341a3572a2354902121f18
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-26 10:58:51 -0800

    Test current_user response for guest user

--- spec/controllers/current_user_controller_spec.rb
+++ spec/controllers/current_user_controller_spec.rb
@@ -6,11 +6,18 @@ RSpec.describe CurrentUserController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
   describe '#show' do
-    it 'returns a json response' do
+    it 'returns a json response for user' do
       sign_in user
       get :show
 
-      expect(response.body).to include("test@test.com")
+      expect(response.parsed_body["email"]).to eq("test@test.com")
+    end
+
+    it 'returns a json response for guest user' do
+      sign_out user
+      get :show, as: :json
+
+      expect(response.parsed_body["error"]).to eq("You need to sign in or sign up before continuing.")
     end
   end
 end
