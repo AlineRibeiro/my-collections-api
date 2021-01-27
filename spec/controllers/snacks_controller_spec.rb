b781225e431ca8b60065501ea6e01ef8ab148816
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-27 10:14:52 -0800

    Authentication tests (#4)
    * Configure helpers to accept rspec devise tests
    
    * Test current_user response for user
    
    * Test current_user response for guest user
    
    * Code cleanup
    
    * Test parsed body of snacks index

--- spec/controllers/snacks_controller_spec.rb
+++ spec/controllers/snacks_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SnacksController, type: :controller do
   describe '#index' do
     it 'returns a json response' do
       get :index
-      expect(response.body).to include('this is the snacks index')
+      expect(response.parsed_body["snacks"]).to eq('this is the snacks index')
     end
   end
 end
