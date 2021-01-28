d4ea6bd907853d81b7221d56411c82f458ddd3ae
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-28 15:51:27 -0800

    Test if guest user is able to create a snack

--- spec/controllers/snacks_controller_spec.rb
+++ spec/controllers/snacks_controller_spec.rb
@@ -4,9 +4,34 @@ require 'rails_helper'
 
 RSpec.describe SnacksController, type: :controller do
   describe '#index' do
-    it 'returns a json response' do
-      get :index
-      expect(response.parsed_body['snacks']).to eq('this is the snacks index')
+    # it 'returns a json response' do
+    #   get :index
+    #   expect(response.parsed_body['snacks']).to eq('this is the snacks index')
+    # end
+  end
+
+  describe '#create' do
+    context 'with guest user' do
+      it 'returns an error' do
+        post :create, params:  { snack: { name: 'TestSnack', description: 'This is the description for test snack'} }, as: :json
+        expect(response.parsed_body['error']).to eq('You need to sign in or sign up before continuing.')
+      end
+    end
+    context 'with user and valid params' do
+      # it 'returns an error' do
+      #   post :create, params:  { snack: { name: 'TestSnack', description: 'This is the description for test snack'} }, as: :json
+      #   expect(response.parsed_body['error']).to eq('You need to sign in or sign up before continuing.')
+      # end
+    context 'with user and invalid params' do
+      # it 'returns an error' do
+      #   post :create, params:  { snack: { name: 'TestSnack', description: 'This is the description for test snack'} }, as: :json
+      #   expect(response.parsed_body['error']).to eq('You need to sign in or sign up before continuing.')
+      # end
+    end
     end
   end
 end
+
+# context - with user
+# teste para user logado parametros validos - consegue criar um snack sem problemas
+# teste para user logado, mas com parametros invalidos - ver se api retorna uma mensagem de erro
