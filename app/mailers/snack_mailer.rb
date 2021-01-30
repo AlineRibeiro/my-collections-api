ae8cdcab7bda12736a04cb4dd114a5591082e057
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-29 19:08:46 -0800

    Rename mailer method

--- app/mailers/snack_mailer.rb
+++ app/mailers/snack_mailer.rb
@@ -6,11 +6,11 @@ class SnackMailer < ApplicationMailer
     mail_list = admins.map { |admin| admin.email }
 
     mail_list.each do |email|
-      send_snack_creation(email, snack).deliver_now
+      notify_snack_creation(email, snack).deliver_now
     end
   end
 
-  def send_snack_creation(email, snack)
+  def notify_snack_creation(email, snack)
     @email = email
     @snack_name = snack.name
     mail(to: email, subject: "Snack #{@snack_name} has been created")
