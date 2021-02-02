969b2708f4ecb1d7e4bb7ab2b8bf297dbe53f93a
Aline Ribeiro <alinear.mail@gmail.com>
2021-02-01 20:13:24 -0800

    Only authorize admins to view sidekiq panel

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   resource :current_user, only: :show, controller: :current_user
 
   resources :collections, only: %i[create update show index]
-
-  mount Sidekiq::Web => "/sidekiq"
 end
