334d32dc1f09c2b524052c1dafc8763a801ea3fa
Aline Ribeiro <alinear.mail@gmail.com>
2021-02-05 17:16:01 -0800

    Create a factory for CollectionItem

---
+++ spec/factories/collection_items.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :collection_item do
+    name { "CollectionItem name" }
+    description { "This is the description for my CollectionItem" }
+    maker { "MyMaker" }
+    paid_price { 100.00 }
+    current_value { 200.00 }
+    serial_number { "Serial1020" }
+    origin { "MyOrigin" }
+    circa { 2010 }
+    condition { 1 }
+    signed { false }
+    collection
+  end
+end
