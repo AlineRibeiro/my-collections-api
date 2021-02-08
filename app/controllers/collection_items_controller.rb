fd9a2d96a604a390bbfed32917cb402a6e511fe5
Aline Ribeiro <alinear.mail@gmail.com>
2021-02-08 10:53:46 -0800

    Show collection_item

--- app/controllers/collection_items_controller.rb
+++ app/controllers/collection_items_controller.rb
@@ -18,6 +18,17 @@ class CollectionItemsController < ApplicationController
     end
   end
 
+  def show
+    collection_item_id = params[:id]
+    collection_id = params[:collection_id]
+    collection_item = CollectionItem.find_by(collection_id: collection_id, id: collection_item_id)
+    if collection_item
+      render json: collection_item
+    else
+      render json: { errors: 'This item does not exist' }
+    end
+  end
+
   private
 
   def collection_item_params
