f9c8b4c86c11ddfca7c95e6a816d5af3d6df5ff1
Aline Ribeiro <alinear.mail@gmail.com>
2021-02-18 15:42:26 -0800

    Fix length validation

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,5 +4,5 @@ class Hashtag < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
-  validates :description, length: { minimum: 4 }
+  validates :name, length: { minimum: 4 }
 end
